24d4edbd55533c880a2c2b79a6b75e55a73fd0c9
Josh <jsjf93@hotmail.co.uk>
2018-04-13 09:52:55 +0100

    Weka API implemented
    Outputs basic statistics and confusion matrix

--- TabDifficultyAnalyser/src/tabdifficultyanalyser/Main.java
+++ TabDifficultyAnalyser/src/tabdifficultyanalyser/Main.java
@@ -6,6 +6,13 @@
  */
 package tabdifficultyanalyser;
 
+import java.io.FileReader;
+import java.util.Random;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+
 /**
  *
  * @author Joshua Foster
@@ -18,8 +25,33 @@ public class Main{
         TabDatabase db = new TabDatabase();
         // Read all the tabs into the database
         db.readInTabDatabase(rootFolder);
-        // Print out the size of the database
-        System.out.println(db.getSize());
+        // Generate the ARFFs
+        generateArffs(db);
+        
+        
+        // Load data
+        String filePath = "combined.arff";
+        Instances data = loadData(filePath);
+        
+        // Create instance of Naive Bayes classifier and Evaluation object
+        NaiveBayes nb = new NaiveBayes();
+        Evaluation e = new Evaluation(data);
+        // Number of folds for cross validation
+        int folds = 10;
+        // Cross validate
+        e.crossValidateModel(nb, data, folds, new Random(1));
+        // Output results
+        System.out.println(e.toSummaryString());
+        System.out.println(e.toMatrixString("=== Confusion Matrix ==="));
+        
+        
+    }
+    
+    /**
+     * Generates all of the ARFF files
+     * @param db the database of tabs
+     */
+    public static void generateArffs(TabDatabase db){
         // Create an instance of the FeatureExtractor
         FeatureExtractor ft = new FeatureExtractor();
         // Create the ARFFs
@@ -33,5 +65,24 @@ public class Main{
         ft.totalNoteCount(db);
         ft.numberOfBars(db);
         ft.combined(db);
-    } 
+    }
+    
+    /**
+     * A method that takes a file path as a String and reads the data into an
+     * Instances object.
+     * @param path
+     * @return i
+     */
+    public static Instances loadData(String path){
+        Instances i = null;
+        try{
+            FileReader fr = new FileReader(path);
+            i = new Instances(fr);
+            i.setClassIndex(i.numAttributes()-1);
+        }
+        catch(Exception e){
+            System.out.println("Unable to read file. Exception: " + e);
+        }
+        return i;
+    }
 }
